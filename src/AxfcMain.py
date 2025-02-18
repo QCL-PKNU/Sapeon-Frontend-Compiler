52dd41da958149b155e26d1948f355e641f8391f
povkimsay <povkimsay@gmail.com>
2025-02-18 10:41:45 +0000

    Update laboratory name

--- src/AxfcMain.py
+++ src/AxfcMain.py
@@ -7,7 +7,7 @@
 #      Youngsun Han (youngsun@pknu.ac.kr)
 #      Heng Sengthai (sengthai37@gmail.com)
 #
-#   High Performance Computing Laboratory (hpcl.pknu.ac.kr)
+#   Quantum Computing Labaratory (qcl.pknu.ac.kr)
 #######################################################################
 
 import argparse
@@ -67,51 +67,51 @@ def __main(params):
         logging.error("Error] Compile TF graph to AXIGraph: %s", err)
         return err
 
-    # Save the generated AIXGraphs
-    if out_path is None:
-        out_path = os.path.dirname(in_path) + '/aix_graph.out'
+    # # Save the generated AIXGraphs
+    # if out_path is None:
+    #     out_path = os.path.dirname(in_path) + '/aix_graph.out'
     
-    err = fc.dump_aix_graphs(out_path, aix_graphs, aix_graph_format)
-    if err is not AxfcError.SUCCESS:
-        logging.error("Error] Dump out AIXGraphs: %s", err)
-        return err
-
-    # # AIX custom model
-    # dir = os.path.dirname(os.path.realpath('__file__'))
-    # tst_path = dir + '/tst'
-    # aix_graph_path = tst_path + '/aix_graph.out.'
-    # print(aix_graph_path)
-    
-    # err, custom_model_path = fc.dump_custom_model(path=in_path,
-    #                            kernel_path=kernel_path,
-    #                            aix_graph_path=aix_graph_path,
-    #                            save_path=os.path.dirname(in_path))
-
+    # err = fc.dump_aix_graphs(out_path, aix_graphs, aix_graph_format)
     # if err is not AxfcError.SUCCESS:
-    #     logging.error("Error] Dump out AIX custom model: %s", err)
+    #     logging.error("Error] Dump out AIXGraphs: %s", err)
+    #     return err
 
-    # AIX Launcher: TESTING
-    # if kernel_path is not None:
-    #
-    #     custom_model_path = '../tst/custom_model.pb'
-    #     output = fc.dump_launcher(custom_model_path=custom_model_path,
-    #                               kernel_op_path=kernel_path)
-    #     print('Prediction:')
-    #     print(output)
+    # # # AIX custom model
+    # # dir = os.path.dirname(os.path.realpath('__file__'))
+    # # tst_path = dir + '/tst'
+    # # aix_graph_path = tst_path + '/aix_graph.out.'
+    # # print(aix_graph_path)
+    
+    # # err, custom_model_path = fc.dump_custom_model(path=in_path,
+    # #                            kernel_path=kernel_path,
+    # #                            aix_graph_path=aix_graph_path,
+    # #                            save_path=os.path.dirname(in_path))
 
-    # for AIXIR graph viewer
-    # if gv_path is not None:
-    #     aix_ir_graph = fc.get_ir_graph()
-    #     err = aix_ir_graph.dump_to_file(gv_path, ["Const", "Pad"])
-    #     if err is not AxfcError.SUCCESS:
-    #         logging.error("Error] Dump out AIXIR graph: %s", err)
-    #         return err
+    # # if err is not AxfcError.SUCCESS:
+    # #     logging.error("Error] Dump out AIX custom model: %s", err)
 
-    logging.info("##########################")
-    logging.info("# Finished Compilation")
-    logging.info("##########################")
+    # # AIX Launcher: TESTING
+    # # if kernel_path is not None:
+    # #
+    # #     custom_model_path = '../tst/custom_model.pb'
+    # #     output = fc.dump_launcher(custom_model_path=custom_model_path,
+    # #                               kernel_op_path=kernel_path)
+    # #     print('Prediction:')
+    # #     print(output)
 
-    return AxfcError.SUCCESS
+    # # for AIXIR graph viewer
+    # # if gv_path is not None:
+    # #     aix_ir_graph = fc.get_ir_graph()
+    # #     err = aix_ir_graph.dump_to_file(gv_path, ["Const", "Pad"])
+    # #     if err is not AxfcError.SUCCESS:
+    # #         logging.error("Error] Dump out AIXIR graph: %s", err)
+    # #         return err
+
+    # logging.info("##########################")
+    # logging.info("# Finished Compilation")
+    # logging.info("##########################")
+
+    # return AxfcError.SUCCESS
 
 
 if __name__ == "__main__":
